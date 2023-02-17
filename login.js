3900d13d2afa4db22d378d4d22b5deef17fdce20
valdrinhajra1 <125669310+valdrinhajra1@users.noreply.github.com>
2023-02-17 14:47:34 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,12 @@
+function valido(){
+        var email=document.getElementById('useri').value ; 
+        var password=document.getElementById('passi').value; //
+      if(email == ""){ 
+        alert("Email eshte e zbrazet"); //
+     } 
+      else if(password == ""){ 
+        alert("Passwordi eshte i zbrazet");
+         }
+         console.log(useri);
+        }
+     
